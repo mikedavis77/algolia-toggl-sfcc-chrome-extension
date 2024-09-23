f8f6e1c38d63607eeb3632e2f75a7709fc0f3bfb
Mike Davis <mikedavisis@gmail.com>
2024-09-23 16:45:55 -0700

    added logo info

--- background.js
+++ background.js
@@ -2,18 +2,29 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.set({ workspaceId: '', apiKey: '' }, () => {});
 });
 
+const icons = {
+  enabled: {
+    16: 'images/enabled-16x16.png',
+    32: 'images/enabled-32x32.png',
+  },
+  disabled: {
+    16: 'images/disabled-16x16.png',
+    32: 'images/disabled-32x32.png',
+  },
+};
+
 const addListenerForAlgoliaSfdcToggl = () => {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
     if (/algolia.lightning.force.com\/lightning\/r\/Case\/[^/]*/i.test(tabs[0].url)) {
       chrome.storage.local.set({ tabCache: {} }, () => {});
       // todo set icon image
-      //chrome.action.setIcon({ path: 'icons/16x16.png' });
+      //chrome.action.setIcon({ path: icons.enabled });
       chrome.action.setPopup({ popup: 'popup.html' });
       chrome.action.setBadgeText({ text: 'ON' });
       chrome.action.setBadgeBackgroundColor({ color: '#4688F1' });
     } else {
       // todo set disabled icon image
-      //chrome.browserAction.setIcon({ path: 'icons/icon-disabled.png' });
+      //chrome.browserAction.setIcon({ path: icons.disabled });
       chrome.action.setPopup({ popup: 'disabled.html' });
       chrome.action.setBadgeText({ text: '' });
     }
